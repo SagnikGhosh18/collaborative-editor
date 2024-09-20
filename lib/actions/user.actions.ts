1b4a8d64daa15609e9aede3aad887c2cdaae9f94
SagnikGhosh18 <sagnikghosh18@gmail.com>
2024-09-20 08:47:08 +0530

    Adding user collab

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,9 +1,10 @@
 "use server";
 
-import { clerkClient } from "@clerk/nextjs/server";
+import { clerkClient, currentUser } from "@clerk/nextjs/server";
 import { parseStringify } from "../utils";
+import { liveblocks } from "../liveblocks";
 
-export const getClerkUsers = async ({ userIds }: { userIds: string[]}) => {
+export const getClerkUsers = async ({ userIds }: { userIds: string[] }) => {
   try {
     const { data } = await clerkClient.users.getUserList({
       emailAddress: userIds,
@@ -16,10 +17,43 @@ export const getClerkUsers = async ({ userIds }: { userIds: string[]}) => {
       avatar: user.imageUrl,
     }));
 
-    const sortedUsers = userIds.map((email)=>users.find((user)=>user.email===email));
+    const sortedUsers = userIds.map((email) =>
+      users.find((user) => user.email === email)
+    );
 
     return parseStringify(sortedUsers);
   } catch (err) {
     console.log(err);
+  }
+};
+
+export const getDocumentUsers = async ({
+  roomId,
+  currentUser,
+  text,
+}: {
+  roomId: string;
+  currentUser: string;
+  text: string;
+}) => {
+  try {
+    const room = await liveblocks.getRoom(roomId);
+
+    const users = Object.keys(room.usersAccesses).filter(
+      (email: string) => email !== currentUser
+    );
+
+    if (text.length) {
+      const lowerCaseText = text.toLowerCase();
+      const filteredUsers = users.filter((email: string) =>
+        email.toLowerCase().includes(lowerCaseText)
+      );
+
+      return parseStringify(filteredUsers);
+    }
+
+    return parseStringify(users);
+  } catch (err) {
+    console.log(`Error fetching document users: ${err}`);
   }
 };
